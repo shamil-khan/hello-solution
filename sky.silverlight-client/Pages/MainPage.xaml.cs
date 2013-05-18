ab4100bb5f4ba2bec01a488cdb41264f8cc1cece
Shamil Khan <shamilkhan@msn.com>
2013-05-18 16:36:31 +0500

    added ria, wcf and duplex services and their silvelight client

---
+++ sky.silverlight-client/Pages/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Sky.ServiceReference1;
+using Sky.ServiceReference2;
+using Sky.Services;
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Sky.Pages
+{
+    public partial class MainPage : Page
+    {
+        string _Responses;
+
+        EchoRiaContext _EchoRiaService;
+        EchoWcfServiceClient _EchoWcfService;
+        EchoDuplexServiceClient _EchoDuplexService;
+        const string _DuplexServicePath = "../services/echo-duplex-service.svc";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ShowExceptionInWindow(() =>
+                {
+                    _EchoRiaService = new EchoRiaContext();
+                    ButtonEchoRiaService.Click += (s, e) => ShowExceptionInWindow( () => _EchoRiaService.Echo(TextRequest.Text, a => UpdateResponse(a.Value), null));
+
+                    _EchoWcfService = new EchoWcfServiceClient("BasicRelative");
+                    _EchoWcfService.EchoCompleted += (s, e) => UpdateResponse(e.Result);
+                    ButtonEchoWcfService.Click += (s, e) => _EchoWcfService.EchoAsync(TextRequest.Text);
+
+                    _EchoDuplexService = new EchoDuplexServiceClient(new PollingDuplexHttpBinding(PollingDuplexMode.MultipleMessagesPerPoll), new EndpointAddress(new Uri(_DuplexServicePath, UriKind.Relative)));
+                    _EchoDuplexService.ReceiveReceived += (s, e) => UpdateResponse(e.value);
+                    _EchoDuplexService.EchoCompleted += (s, e) => UpdateResponse(e.Result);
+                    _EchoDuplexService.RegisterCompleted += (s, e) => UpdateResponse("Echo Duplex Service Register Status : " + e.Result.ToString());
+                    _EchoDuplexService.UnRegisterCompleted += (s, e) => UpdateResponse("Echo Duplex Service Un-Register Status : " + e.Result.ToString());
+                    ButtonEchoDuplexService.Click += (s, e) => ShowExceptionInWindow(() => _EchoDuplexService.EchoAsync(TextRequest.Text));
+                    ButtonEchoDuplexServiceRegister.Click += (s, e) => ShowExceptionInWindow(() => _EchoDuplexService.RegisterAsync(TextRequest.Text));
+                    ButtonEchoDuplexServiceUnRegister.Click += (s, e) => ShowExceptionInWindow(() => _EchoDuplexService.UnRegisterAsync(TextRequest.Text));
+                });
+        }
+
+        public void UpdateResponse(string value)
+        {
+            _Responses = string.Format("<Response>{0}</Response>{1}{2}", value, Environment.NewLine, _Responses);
+            TextResponse.Text = string.Format("<Responses>{1}{0}</Responses>", _Responses, Environment.NewLine);
+        }
+
+        static void ShowExceptionInWindow(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+
+}
--- web-app/default.aspx
+++ web-app/default.aspx
@@ -13,10 +13,21 @@
 <!DOCTYPE >
 <html>
 <head runat="server">
-    <title></title>
+    <title>Shamil Khan - Test Application</title>
+    <link href="css/common.css" rel="stylesheet" />
 </head>
 <body>
     <form id="form1" runat="server">
+    <div class="navbar">
+        <a href="silverlight-client.aspx">Silverlight Page</a>
+        <a href="echo.aspx">Echo Page</a>
+        <a href="/services/product-service.svc/products/10">Products 10</a>
+        <a href="/services/product-service.svc/products/100">Products 100</a>
+        <a href="/services/product-service.svc/product/1215">Product # 1215</a>
+        <a href="/services/echo-service.svc/echo/hello">Echo - Hello</a>
+    </div>
+    <br />
+    <br />
     <div>
         <asp:TextBox ID="TextRequest" Text="Hello" Width="200px" runat="server" />
         <asp:LinkButton ID="LinkEcho" Text="Echo 1" Width="120px" runat="server" />
